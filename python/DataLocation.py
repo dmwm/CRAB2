92c1cac4a12dbbf142b0e96e6c3128bb9f14ec75
belforte <stefano.belforte@cern.ch>
2014-08-11 23:46:18 +0200

    new NodeName - some work in progress

--- python/DataLocation.py
+++ python/DataLocation.py
@@ -24,6 +24,7 @@ class DataLocation:
         self.Listfileblocks = Listfileblocks  # DLS input: list of fileblocks lists
 
         self.cfg_params = cfg_params
+        self.datasetPath = cfg_params['CMSSW.datasetpath']
 
         self.SelectedSites = {}        # DLS output: list of sites hosting fileblocks
                                        # can be retrieved using method getSites
@@ -33,6 +34,13 @@ class DataLocation:
         """
         Contact DLS
         """
+
+        print "SB================================="
+        print "in fetchDLSInfo"
+        print "self.datasetPath"
+        print self.datasetPath
+        print "SB================================="
+        
 
         # Force show_prod=1 for everybody, grid jobs rely on central black list and t1access role to limit access to T1's
         self.cfg_params['CMSSW.show_prod'] = 1
@@ -46,9 +54,38 @@ class DataLocation:
         global_dbs3 = "https://cmsweb.cern.ch/dbs/prod/global/DBSReader"
 
         # first try PhEDEx
-        DLS_type="DLS_TYPE_PHEDEX"
-        dls=DLSInfo(DLS_type,self.cfg_params)
-        blockSites = dls.getReplicasBulk(self.Listfileblocks)
+        #DLS_type="DLS_TYPE_PHEDEX"
+        #dls=DLSInfo(DLS_type,self.cfg_params)
+        #blockSites = dls.getReplicasBulk(self.Listfileblocks)
+        import json
+        import subprocess
+        PhEDExUrl = "https://cmsweb.cern.ch/phedex/datasvc/json/prod/BlockReplicaSummary"
+        apiUrl = PhEDExUrl + "?BlockReplicaSummary?dataset=%s&complete=y" % self.datasetPath
+        cmd='curl -ks "https://cmsweb.cern.ch/phedex/datasvc/json/prod/BlockReplicaSummary?dataset=/SingleMu/Run2012B-TOPMuPlusJets-22Jan2013-v1/AOD&complete=y"'
+        j=subprocess.check_output(cmd,shell=True)
+        dict=json.loads(j)
+        # blockLocations is a list of dictionaries, one per block
+        # format of each entry is like
+        # {u'name': u'/SingleMu/Run2012B-TOPMuPlusJets-22Jan2013-v1/AOD#42cbaf9c-715f-11e2-af21-00221959e72f',
+        # u'replica': [{u'complete': u'y', u'node': u'T1_IT_CNAF_MSS'},
+        #      {u'complete': u'y', u'node': u'T3_FR_IPNL'},
+        #      {u'complete': u'y', u'node': u'T1_IT_CNAF_Disk'},
+        #      {u'complete': u'y', u'node': u'T1_US_FNAL_Disk'},
+        #      {u'complete': u'y', u'node': u'T2_US_Purdue'},
+        #      {u'complete': u'y', u'node': u'T1_IT_CNAF_Buffer'}]}
+
+        blockLocations=dict['phedex']['block']
+
+        # convert to the blockSites format required by this code
+        blockSites={}
+        for block in blockLocations:
+            bname = str(block['name'])
+            blockSites[bname]=[]
+            for replica in block['replica']:
+                print replica
+                site=str(replica['node'])
+                blockSites[bname].append(site)
+
         if len(blockSites) == 0 :
             common.logger.info("No dataset location information found in PhEDEx")
             if dbs_url == global_dbs3:
