39e7e3e8c832248def1246101d5fc6641d9373bc
Stefano Belforte <stefano.belforte@cern.ch>
2014-08-18 15:39:45 +0200

    2_10_7 not 2_10_7_pre3

--- python/common.py
+++ python/common.py
@@ -13,7 +13,7 @@
 
 prog_name = 'crab'
 prog_version = (2, 10, 7)
-prog_tag = 'pre3'
+prog_tag = ''
 prog_version_str=`prog_version[0]`+'.'+`prog_version[1]`+'.'+`prog_version[2]`
 if prog_tag and len(prog_tag)>0:
     prog_version_str+= "_%s" % prog_tag
